6cc5e5d251af81068126e7b8c000ce3ac4cc9831
alex <alex@tekartik.com>
2021-06-29 11:14:58 +0200

    fix: ci

--- repo_support/tool/run_ci.dart
+++ repo_support/tool/run_ci.dart
@@ -2,14 +2,14 @@ import 'package:dev_test/package.dart';
 import 'package:path/path.dart';
 
 Future main() async {
-  /*
   for (var dir in [
     'app_platform',
     'app_prefs',
     'app_sqflite',
+    'app_sembast',
     join('example', 'sqflite_test_app'),
   ]) {
     await packageRunCi(join('..', 'packages', dir));
-  }*/
-  await packageRunCi(join('..'), recursive: true);
+  }
+  //await packageRunCi(join('..'), recursive: true);
 }
