edd046cdbbc3d80873eb5b3c5a5ce9d2bd20f275
Alexandre Roux <alex@tekartik.com>
2023-12-20 10:06:24 +0100

    fix ci

--- packages/app_sembast/tool/run_ci_override.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dev_test/package.dart';
-import 'package:process_run/shell.dart';
-
-Future main() async {
-  await packageRunCi('.',
-      options: PackageRunCiOptions(noOverride: true, noTest: true));
-  await run('dart test -j 1');
-}
