8e23f39ba572da045d0231284b94db6fd782cd90
Alexandre Roux <alex@tekartik.com>
2024-07-20 22:03:05 +0200

    move cmd tools to https://github.com/tekartikprj/io_tools

--- packages/cmd_echo/bin/echo.dart
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env dart
-
-import 'package:tekartik_cmd_echo/cmd_echo.dart' as cmd_echo;
-
-Future main(List<String> arguments) => cmd_echo.main(arguments);
--- packages/cmd_record/lib/src/import.dart
+++
@@ -1,2 +0,0 @@
-// ignore: depend_on_referenced_packages
-export 'package:tekartik_common_utils/common_utils_import.dart';
